fe5c32d119217d12a6d6af5d90e6326bd69e3f1d
Xinchang_WANG <627684875@qq.com>
2021-04-09 22:49:53 +0800

    set app container height

--- src/App.vue
+++ src/App.vue
@@ -1,46 +1,38 @@
 <template>
-  <div id="app">
-    <el-container >
+  <!-- <div id="app"> -->
+    <el-container id="app">
       <router-view></router-view>
     </el-container>
-  </div>
+  <!-- </div> -->
 </template>
 
 <script>
-
-
-
-
-  export default {
-    name: 'App',
-    data(){
-      return{
-        NavMenuState:''
-      }
+export default {
+  name: "App",
+  data() {
+    return {
+      NavMenuState: "",
+    };
+  },
+  components: {},
+  methods: {
+    //获取NavMenu的key
+    GetNavMenuKey: function (key) {
+      this.NavMenuState = key;
     },
-    components: {
 
-
-
-    },
-    methods:{
-      //获取NavMenu的key
-      GetNavMenuKey:function (key) {
-        this.NavMenuState=key
-      },
-
-      //路由判断
-
-
-    }
-
-
-
-  }
+    //路由判断
+  },
+};
 </script>
 
 <style>
-  #app {
+html,
+body,
+#app{
+  margin: 0;
+  height: 100%;
+  width: 100%;
+}
 
-  }
 </style>
